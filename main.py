77f8b1aa571610697321568bbac0ad5df91e9812
OlgaPlesco <olga.plesco@gmail.com>
2021-08-11 10:40:43 +0300

    warnings resolved

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
-from board import full_board
 from actions import yes_or_no
 from actions import moves
 from players import get_player_details
 
 
-
 def game():
-    play_or_not = yes_or_no ("Do you want to play?")
+    play_or_not = yes_or_no("Do you want to play?")
 
     if play_or_not == "y":
-        print ("Game started.\n")
+        print("Game started.\n")
         get_player_details(first_player_list=True, second_player_list=False)
-        # current_player = player_details[0]
-        # player_details = get_player_details(current_player)
+# current_player = player_details[0]
+#       # player_details = get_player_details(current_player)
         moves()
     else:
         print("Bye!")
 
 
 if __name__ == '__main__':
-  game()
+    game()
--- players.py
+++ players.py
@@ -1,27 +1,25 @@
-from actions import yes_or_no
-
-def name_selection():
-    mark_selection = yes_or_no ("Do you want to be Alfa and play X today?")
-    if name_selection == "y":
-        print ("Nice choise, Alfa. You will mark with X in this game.\n")
-        current_player_list.append("Alfa")
-    else:
-        print ("Beta is also a nice nick. Don't forget that you mark with O.\n")
-        current_player_list.append("Beta")
+#def name_selection():
+#  mark_selection = yes_or_no ("Do you want to be Alfa and play X today?")
+#   if name_selection == "y":
+#    print ("Nice choise, Alfa. You will mark with X in this game.\n")
+#  current_player_list.append("Alfa")
+#  else:
+#    print ("Beta is also a nice nick. Don't forget that you mark with O.\n"
+#  current_player_list.append("Beta")
 
 def get_player_details(first_player_list, second_player_list):
     first_player_list = []
     second_player_list = []
     name = input("Plase type your name.\n")
     mark = input("Please select the mark to play with: X or O.")
-   # winner_combination = ("")
+# winner_combination = ("")
     if mark  == "x" or mark == "X":
       first_player_list.append(name)
       first_player_list.append(mark)
     else:
       second_player_list.append(name)
       second_player_list.append(mark)
-   # print(first_player_list, second_player_list)
+## print(first_player_list, second_player_list)
     print("Nice to have you here, ", name,". Your selected mark is ", mark,".\n")
 
 
