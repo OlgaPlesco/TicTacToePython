e4b5122eb1800700168981a3bff095e9c8f6d704
OlgaPlesco <olga.plesco@gmail.com>
2021-08-25 08:19:29 +0300

    regame loop

--- main.py
+++ main.py
@@ -6,33 +6,34 @@ from actions import game_moves
 from players import x_game_moves
 from players import y_game_moves
 from actions import check_win
-from actions import check_position
 
 
 def game():
     play_or_not = yes_or_no("Do you want to play?")
-
 
     if play_or_not == "y":
         print("Game started.\n")
         moves()
         check_win()
 
-        while len(game_moves) != 9:
+        while len(game_moves) <= 9:
 
-            if check_win() is False:
+            if not check_win():
                 flip_players()
                 print(game_moves, x_game_moves, y_game_moves)
                 check_win()
             else:
                 print(f"Congratulations, {Players.current_player_mark}!")
+                game_moves.clear()
+                x_game_moves.clear()
+                y_game_moves.clear()
                 break
         else:
             print("draw")
 
-
     else:
         print(f"Bye, {Players.name}!")
+
 
 def regame():
     replay_or_not = yes_or_no("Would you repete the experience?")
@@ -41,6 +42,10 @@ def regame():
     else:
         print(f"Bye, {Players.name}!")
 
+
 if __name__ == '__main__':
     game()
-    regame()
+    response = yes_or_no("First round is over. You can play more. Select Y or N.")
+    while True:
+        regame()
+        break
